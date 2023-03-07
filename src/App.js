7d853bf5ff3c42cc0c87d18608261fd3f7224e8a
Paul Ivan Dela Cruz <kmpkhai001@gmail.com>
2023-03-07 23:54:56 +0800

    react portfolio

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import './App.css';
-import Navbar from './routes/Navbar';
+import Navbar from './routes/Navbar'
+import Home from './routes/Home'
+
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
+      <Home/>
     </div>
   );
 }
--- src/routes/Navbar.jsx
+++ src/routes/Navbar.jsx
@@ -1,18 +1,31 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './Navbar.css'
 
 const Navbar = () => {
+  
+  const [active,setActive] = useState("nav-lists");
+  const [toggleIcon, setToggleIcon] = useState ("nav-toggle");
+
+    const navToggle = () => {
+      active === 'nav-lists' ? setActive ('nav-lists nav_activate') : setActive ('nav-lists')
+
+    //Toggle Animation
+    
+      toggleIcon === 'nav-toggle' ? setToggleIcon ('nav-toggle toggle') : setToggleIcon ('nav-toggle')
+
+    };
+
   return (
     <>
     <nav className="nav">
-        <a href='#' className='navlinks'>PIDC</a>
-        <ul>
+        <a href='#' className='navlinks'>POL</a>
+        <ul className={active}>
             <li className='navMenu'><a href='#' className='nav-items'>Home</a></li>
             <li className='navMenu'><a href='#' className='nav-items'>Project</a></li>
             <li className='navMenu'><a href='#' className='nav-items'>About</a></li>
             <li className='navMenu'><a href='#' className='nav-items'>Contact</a></li>
         </ul>
-        <div className="nav-toggle">
+        <div onClick={navToggle} className={toggleIcon}>
             <div className="navline1"></div>
             <div className="navline2"></div>
             <div className="navline3"></div>
