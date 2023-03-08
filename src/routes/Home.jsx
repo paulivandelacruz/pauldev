c4bb6eb080a68cf97fa1ab8c0f2180eb22be6cf8
Paul Ivan Dela Cruz <kmpkhai001@gmail.com>
2023-03-08 16:18:29 +0800

    update

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import './Home.css'
+import Img from '../images/web-development.webp'
 
 const Home = () => {
+  
   return (
-    <div>Home</div>
+    <div className = 'my-container'>
+      <div className='left-container'>
+        <div className='left-container-wrap'>
+          <h2 className='my-first-p'>Hello I'm</h2>
+          <h1 className='my-name'>Paul Ivan Dela Cruz</h1>
+          <p className='intro-p'>Reach me out if you need website for your business.</p>
+        </div>
+      </div>
+      <div className='right-container'>
+        <div className='my-bg1'></div>
+        <img src={Img} alt='' className='image-1'></img>
+      </div>
+    </div>
   )
 }
 
