46cc3d446f0f65690b6852b68d18b761af00621a
Paul Ivan Dela Cruz <kmpkhai001@gmail.com>
2023-04-04 00:33:07 +0800

    project feature added

--- src/routes/Project.jsx
+++ src/routes/Project.jsx
@@ -1,14 +1,65 @@
 import React from 'react'
+import { motion } from 'framer-motion'
+import { fadeIn } from '../variants'
+
+//images
+import img1 from '../images/portfolio-image-1.png'
+import img2 from '../images/portfolio-image-2.png'
+import img3 from '../images/portfolio-image-3.png'
 
 const Project = () => {
   return (
     <section className='section' id='project'>
       <div className='container mx-auto'>
-      <div className='text-text'>Project
-      </div>
-      </div>
+        <div className='text-text flex flex-col lg:flex-row gap-x-10'>
+          <div className='flex-1 flex flex-col mb-10 lg:mb-0'>
+            
+            <div>
+              <h2 className='h2 leading-tight text-accent lg:text-[70px]'>My Latest<br/>Projects
+              </h2>
+              <p className='max-w-sm mb-16 font-text2'>
+                Here is my latests works and sample projects this is just a basic styling and simple designs and will be change once I've created real projects
+              </p>
+            </div>
+            
+            
+            
+          <div className='group relative overflow-hidden  border-2 border-black rounded-xl'>
+              <div className='group-hover:bg-black/70 w-full h-full absolute z-40 transition-all duration-300'></div>
+                <img className='group-hover:scale-125 transition-all duration-500' src={img1} alt='' />
+                <div className='absolute -bottom-full left-12 group-hover:bottom-24 transition-all duration-500 z-50'>
+                  <span className='text-gradient font-text'>UI Design</span>
+                  </div>
+                <div className='absolute -bottom-full left-12 group-hover:bottom-14 transition-all duration-700 z-50'>
+                  <span className='text-3xl text-white'>Sample Portfolio</span>
+                </div>
+            </div>
+          </div>
+              <div className='flex-1 flex flex-col gap-y-10'>
+              <div className='group relative overflow-hidden  border-2 border-black rounded-xl'>
+                <div className='group-hover:bg-black/70 w-full h-full absolute z-40 transition-all duration-300'></div>
+                  <img className='group-hover:scale-125 transition-all duration-500' src={img2} alt='' />
+                  <div className='absolute -bottom-full left-12 group-hover:bottom-24 transition-all duration-500 z-50'>
+                    <span className='text-gradient font-text'>UI/UX Design</span>
+                    </div>
+                  <div className='absolute -bottom-full left-12 group-hover:bottom-14 transition-all duration-700 z-50'>
+                    <span className='text-3xl text-white'>Sample Valorant Web</span>
+                  </div>
+                </div>
 
-
+            <div className='group relative overflow-hidden  border-2 border-black rounded-xl'>
+              <div className='group-hover:bg-black/70 w-full h-full absolute z-40 transition-all duration-300'></div>
+                <img className='group-hover:scale-125 transition-all duration-500' src={img3} alt='' />
+                <div className='absolute -bottom-full left-12 group-hover:bottom-24 transition-all duration-500 z-50'>
+                  <span className='text-gradient font-text'>Graphic Design</span>
+                  </div>
+                <div className='absolute -bottom-full left-12 group-hover:bottom-14 transition-all duration-700 z-50'>
+                  <span className='text-3xl text-white'>Sample Graphic Designs</span>
+                </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </section>
     
   )
