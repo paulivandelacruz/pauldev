ff207d2281441e7f8ba4a07022ee4428b9a6002d
Paul Ivan Dela Cruz <kmpkhai001@gmail.com>
2023-12-01 01:29:06 +0800

    feat: update on projects

--- src/routes/Project.jsx
+++ src/routes/Project.jsx
@@ -7,6 +7,7 @@ import img1 from '../images/cleaningly.png'
 import img2 from '../images/portfolio-image-6.png'
 import img3 from '../images/portfolio-image-5.png'
 import img4 from '../images/portfolio-image-4.png'
+import img5 from '../images/portfolio-image-7.jpg'
 
 
 
@@ -78,6 +79,7 @@ const Project = () => {
                     </div>
                 </div>
           </motion.div>
+
           </div>
 
               <motion.div 
@@ -85,7 +87,7 @@ const Project = () => {
                 initial='hidden'
                 whileInView={'show'}
                 viewport={{once: true, amount: 0.3}}
-                className='flex-1 flex flex-col gap-y-10'
+                className='flex-1 flex flex-col gap-y-[34px]'
               >
               <div className='group relative overflow-hidden  border-2 border-black rounded-xl'>
                 <div className='group-hover:bg-black/70 w-full h-full absolute z-40 transition-all duration-300'></div>
@@ -100,7 +102,7 @@ const Project = () => {
                   <div className='absolute -bottom-full left-6 lg:left-12 group-hover:bottom-4 transition-all duration-1000 z-50 text-white'>
                     <div className='flex items-center gap-x-1 max-w-max mx-auto'>
                       <h3 className='hover:text-blue-300'>
-                        View Website
+                        <a href='#'>View Website</a>
                       </h3>
                     </div>
                   </div>
@@ -127,6 +129,24 @@ const Project = () => {
                   </div>
                 </div>
 
+                <div className='group relative overflow-hidden  border-2 border-black rounded-xl'>
+                  <div className='group-hover:bg-black/70 w-full h-full absolute z-40 transition-all duration-300'></div>
+                  <img className='group-hover:scale-125 transition-all duration-500' src={img5} alt='' />
+                    <div className='absolute -bottom-full left-6 lg:left-12 group-hover:bottom-24 transition-all duration-500 z-50'>
+                      <span className='text-gradient font-text'>Web Development</span>
+                      </div>
+                    <div className='absolute -bottom-full left-6 lg:left-12 group-hover:bottom-7 transition-all duration-700 z-50'>
+                      <span className='text-2xl lg:text-3xl text-white'>SuperWheels<br/>Car Dealership</span>
+                    </div>
+
+                    <div className='absolute -bottom-full left-6 lg:left-12 group-hover:bottom-1 transition-all duration-1000 z-50 text-white'>
+                      <div className='flex items-center gap-x-1 max-w-max mx-auto'>
+                        <h3 className='hover:text-blue-300'>
+                          <a href='#'>View Website</a>
+                        </h3>
+                      </div>
+                  </div>
+                </div>
           </motion.div>
         </div>
       </div>
